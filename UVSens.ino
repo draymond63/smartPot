b3a80716ef4340c6526385942039604e4a469f06
yccgabby <59523545+yccgabby@users.noreply.github.com>
2020-02-15 18:02:02 -0500

    moisture and UV sensor

--- UVSens.ino
+++ UVSens.ino
@@ -1,9 +1,46 @@
+    /* 
+ ML8511 UV Sensor
+ By: SmartPot team
+ Date: February 15th, 2020
+ License: Beerware 
+ Note: If we do ever meet Nathan Seidle, we must buy him a beer
+*/
+
+//Hardware pin definitions
+int UVOUT = A2; //Output from the sensor
 
 void UVSetup() {
-  
+  pinMode(UVOUT, INPUT);
 }
 
 uint16_t getUV() {
+  int uvLevel = averageAnalogRead(UVOUT);
+
+  //Use the 3.3V power pin as a reference to get a very accurate output value from sensor
+  float outputVoltage = 0.005 * uvLevel;
+
+  float uvIntensity = mapfloat(outputVoltage, 0.99, 2.8, 0.0, 15.0); //Convert the voltage to a UV intensity level
   
-  return 0;
+  return uvIntensity;
+}
+
+//Takes an average of readings on a given pin
+//Returns the average
+int averageAnalogRead(int pinToRead)
+{
+  byte numberOfReadings = 8;
+  unsigned int runningValue = 0; 
+
+  for(int x = 0 ; x < numberOfReadings ; x++)
+    runningValue += analogRead(pinToRead);
+  runningValue /= numberOfReadings;
+
+  return(runningValue);  
+}
+
+//The Arduino Map function but for floats
+//From: http://forum.arduino.cc/index.php?topic=3922.0
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
--- moistSens.ino
+++ moistSens.ino
@@ -1,9 +1,26 @@
+/*
+* ESP8266 Soil Moisture Sensor
+* By: SmartPot Team 
+* Date: February 15th, 2020
+*/
+
+#define SensorPin A0 
+uint16_t sensorValue = 0; 
 
 void MSSetup() {
-  
+
 }
 
 uint16_t getMoisture(void) {
-  
-  return 0;
+  sensorValue = constrain(map(analogRead(SensorPin), 0, 1023, 0, 100), 0, 100);
+  Serial.println(sensorValue); 
+  if (sensorValue > 80) {
+    return 0;
+  }
+  else if (sensorValue > 40) {
+    return 1;
+  }
+  else {
+    return 2;
+  }
 }
